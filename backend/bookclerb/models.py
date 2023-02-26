95fe0c516b9aa26e16b0e1ef4c9cfdbf561993ce
Ranna Bansal <ranna.bansal@loan-street.com>
2023-02-26 16:44:55 -0500

    Working on book clerb app, added zohaib dancing lol

--- backend/bookclerb/models.py
+++ backend/bookclerb/models.py
@@ -3,11 +3,15 @@ from django.db import models
 class BookClub(models.Model):
     name = models.CharField(max_length=200)
     
-    
-class BookClubSession(models.Model):
-    session_date = models.DateField()
-    
-    
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
+    
+    
+class BookClubSession(models.Model):
+    book_club = models.ForeignKey(BookClub, on_delete=models.CASCADE)
+    session_date = models.DateField()
+    book = models.ForeignKey(Book, blank=True, null=True, default=None, on_delete=models.SET_NULL)
+    
+
     
