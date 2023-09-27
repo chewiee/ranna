dcb9691d98af4725b047b57c298c37fc6f10b486
Ranna Bansal <ranna.bansal@loan-street.com>
2023-09-26 23:38:55 -0400

    api done for todo

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,8 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # From the internet (except for Django stuff)
+    'corsheaders',
+    'rest_framework',
+    
     'bookclerb.apps.BookclerbConfig',
     'polls.apps.PollsConfig',
+    'todo.apps.TodoConfig',
     
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,9 +48,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # From the internet (except for Django stuff)
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -126,3 +135,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
